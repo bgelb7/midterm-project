3a9157144b1624143dfcaab47db0cc862d5cf8e0
Benjamin Gelb <bengelb7@gmail.com>
2022-11-30 19:56:54 +0100

    second take

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,35 @@
-async function getProjects() {
+async function getProjects(project) {
+
     let url = 'https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects-v2';
     try {
-        let res = await fetch(url);
+        let promise1 = await fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects-v2');
         return await res.json();
     } catch (error) {
         console.log(error);
     }
 
+
+
+
+}
+
+
+
+function getProjects(project) {
+
+    let promise1 = fetch('https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects-v2')
+    promise1.then(function (response) {
+        console.log(response);
+    })
+}
+
+getProjects(project1)
+
+
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
 }
